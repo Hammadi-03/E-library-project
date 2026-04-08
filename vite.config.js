fca454cbb21d4f7d0fd6127878dd94eb993d9eb7
Hammadi-03 <[200~abdulazizhammadi809@gmail.com>
2026-04-08 10:02:08 +0700

    feat: add branding assets and configure public directory in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
                 'resources/js/app.js', 
                 'resources/js/react-notifications.tsx'
             ],
+            publicDirectory: 'htdocs',
             refresh: true,
         }),
         react(),
